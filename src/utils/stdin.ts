d4ccf4afd5f7386516504be7f4f122ba5fb275ed
Saix <bark@bigstinky.dog>
2024-11-02 22:40:34 -0400

    add stdin helpers

---
+++ src/utils/stdin.ts
@@ -0,0 +1,24 @@
+import { TextLineStream, TextDelimiterStream, toText,  } from "@std/streams";
+
+export const readLineByLine = (
+  file: ReadableStream<Uint8Array> = Deno.stdin.readable
+) => {
+  return file
+    .pipeThrough(new TextDecoderStream()) // decode Uint8Array to string
+    .pipeThrough(new TextLineStream()); // split string line by line
+};
+
+export const readByDelimiter = (
+  delim: string,
+  file: ReadableStream<Uint8Array> = Deno.stdin.readable
+) => {
+  return file
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextDelimiterStream(delim));
+};
+
+export const readAllStdin = (
+  file: ReadableStream<Uint8Array> = Deno.stdin.readable,
+) => {
+  return toText(file.pipeThrough(new TextDecoderStream()))
+}
